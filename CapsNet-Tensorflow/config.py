9b18bfc53092063db5456d6a7fecd431cea0c99a
Mohit Arvind Khakharia <mohitkhakharia@gmail.com>
2017-12-07 15:10:34 -0500

    had to update the config for affNIST and updated the accuracy

--- CapsNet-Tensorflow/config.py
+++ CapsNet-Tensorflow/config.py
@@ -13,7 +13,7 @@ flags.DEFINE_float('m_minus', 0.1, 'the parameter of m minus')
 flags.DEFINE_float('lambda_val', 0.5, 'down weight of the loss for absent digit classes')
 
 # for training
-flags.DEFINE_integer('batch_size', 128, 'batch size')
+flags.DEFINE_integer('batch_size', 32, 'batch size')
 flags.DEFINE_integer('epoch', 30, 'epoch')
 flags.DEFINE_integer('iter_routing', 3, 'number of iterations in routing algorithm')
 flags.DEFINE_boolean('mask_with_y', True, 'use the true label to mask out target capsule or not')
@@ -38,7 +38,7 @@ flags.DEFINE_string('results', 'results', 'path for saving results')
 #   distributed setting    #
 ############################
 flags.DEFINE_integer('num_gpu', 2, 'number of gpus for distributed training')
-flags.DEFINE_integer('batch_size_per_gpu', 128, 'batch size on 1 gpu')
+flags.DEFINE_integer('batch_size_per_gpu', 32, 'batch size on 1 gpu')
 flags.DEFINE_integer('thread_per_gpu', 4, 'Number of preprocessing threads per tower.')
 
 cfg = tf.app.flags.FLAGS
